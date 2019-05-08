46c2004853c1ce45e435c782ec71f9a0b6baf679
Elderen Lee <elderen2@gmail.com>
2019-05-08 09:14:46 -0400

    changed button color blue to orange

--- client/index.jsx
+++ client/index.jsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom";
 import PrivateChat from "./component/privateChat.jsx";
 import LobbyChat from "./component/lobbyChat.jsx";
 import axios from "axios";
-import io from 'socket.io-client';
 
 class App extends React.Component {
   constructor(props) {
@@ -11,7 +10,6 @@ class App extends React.Component {
     this.state = {
       wifi: 'WiFi Not Connected',
       user: 'Anonymous',
-      response: false
     }
     this.getInfo = this.getInfo.bind(this);
   }
@@ -23,21 +21,9 @@ class App extends React.Component {
     })
   }
 
-  // componentDidMount() {
-  //   this.getInfo();
-  //   const socket = io();
-  //   var user = this.state.name;
-  //   socket.emit("FromAPI", user)
-  //   // socket.on("FromAPI", (data) => {
-  //   //   console.log("FROM API DATA: ", data);
-  //   //   this.setState({ response: data })
-  //   // });
-  //   socket.on('chat message', function(userMsg){
-  //     console.log('user and message: ', userMsg)
-  //     $('#messages').append($('<li>').text(userMsg));
-  //   });
-
-  // }
+  componentDidMount() {
+    this.getInfo();
+  }
 
   getInfo() {
     axios.get('/network')
@@ -47,12 +33,6 @@ class App extends React.Component {
         })
         console.log('Current Network: ', this.state.wifi)
       })
-    // WifiManager.getCurrentWifiSSID()
-    // .then((ssid) => {
-    //   console.log("Your current connected wifi SSID is " + ssid)
-    // }, () => {
-    //   console.log('Cannot get current SSID!')
-    // })
   }
 
   render() {
@@ -64,7 +44,6 @@ class App extends React.Component {
         
         <div>
           <p>{this.state.wifi === 'WiFi Not Connected' ? this.state.wifi : 'Connected to '+this.state.wifi}</p>
-          <p>Response: {this.state.response ? 'True' : 'False'}</p>
         </div>
 
         <div>
